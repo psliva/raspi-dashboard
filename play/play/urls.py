4922ca068f131c747bac621df4cd77d03c9d80c3
psliva <github@psliva.com>
2014-02-04 23:17:43 -0800

    adding urls.py

---
+++ play/play/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'play.views.index'),
+    # url(r'^play/', include('play.foo.urls')),
+    
+    url(r'^traffic$', 'play.views.traffic'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
